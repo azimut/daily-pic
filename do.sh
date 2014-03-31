a7f51bf0a62242d11e04772f5659245d5a3a5bbb
azimut <nicolas.marguglio@gmail.com>
2014-03-31 04:04:08 -0300

    add deviantart support, fix on log pipes and tabs

--- do.sh
+++ do.sh
@@ -5,7 +5,7 @@ WGET_OPT='--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML
 
 help.usage(){
     cat <<EOF
-Usage: $0 [-hcgiafsmntwb]
+Usage: $0 [-hcgiafsmntwbd]
    -g National Geographic Image of the day
    -a NASA Astronomy Picture Of the Day (APOD)
    -i NASA Image of the Day
@@ -18,6 +18,7 @@ Usage: $0 [-hcgiafsmntwb]
    -w wallbase (rand)
    -b bing (iod)
    -r reddit (wallpaper/imgur)
+   -d deviantart (rand/24h)
 EOF
 }
 
@@ -40,10 +41,29 @@ check_in_path 'wget'
     exit 1
 }
 
+http.get.url.deviantart(){
+    local BASE_URL='http://browse.deviantart.com/customization/wallpaper/'
+    local image_url=$(
+        wget "${WGET_OPT}" -q -O- "${BASE_URL}" | 
+        egrep -o 'data-src="http://[[:alnum:]]+\.deviantart\.net/[[:alnum:]]+/200H/.+/.+/.+/.+/.+/.+\.png"' | 
+        cut -f2 -d'"' | 
+        shuf -n1
+    )
+    image_url=${image_url/200H/}
+    if [[ ! -z $image_url ]]; then
+        echo "${image_url}"
+    fi
+}
+
 # Reference: https://github.com/wmmc/Wallpaper-Downloader
 http.get.url.reddit(){
     local BASE_URL='http://www.reddit.com/r/wallpapers/.json'
-    local image_url=$(wget "${WGET_OPT}" -q -O- "${BASE_URL}" | tr ' ' '\n' | egrep -o 'http://i.imgur.com/[[:alnum:]]+\.(png|jpg)' | shuf -n1)
+    local image_url=$(
+        wget "${WGET_OPT}" -q -O- "${BASE_URL}" | 
+	tr ' ' '\n' | 
+	egrep -o 'http://i.imgur.com/[[:alnum:]]+\.(png|jpg)' | 
+	shuf -n1
+    )
     if [[ ! -z ${image_url} ]]; then
         echo "${image_url}"
     fi
@@ -53,7 +73,10 @@ http.get.url.reddit(){
 http.get.url.bing(){
     local BASE_URL='http://www.bing.com/HPImageArchive.aspx?format=js&n=1&pid=hp&video=0'
     local IMAGE_BASE_URL='http://www.bing.com'
-    local image_url=$(wget "${WGET_OPT}" -q -O- "${BASE_URL}" | sed -E 's/.*"url":"([^"]+)"[,\}].*/\1/g' )
+    local image_url=$(
+        wget "${WGET_OPT}" -q -O- "${BASE_URL}" | 
+	sed -E 's/.*"url":"([^"]+)"[,\}].*/\1/g' 
+    )
     if [[ ! -z ${image_url} ]]; then
         echo "${IMAGE_BASE_URL}""${image_url}"
     fi
@@ -61,7 +84,11 @@ http.get.url.bing(){
 
 http.get.url.wallbase(){
     local BASE_URL='http://wallbase.cc/random'
-    local image_url=$(wget "${WGET_OPT}" -q -O- "${BASE_URL}" | egrep -o 'http://thumbs.wallbase.cc//[[:alpha:]-]+/thumb-[0-9]+.jpg' | shuf -n1)
+    local image_url=$(
+        wget "${WGET_OPT}" -q -O- "${BASE_URL}" | 
+	egrep -o 'http://thumbs.wallbase.cc//[[:alpha:]-]+/thumb-[0-9]+.jpg' | 
+	shuf -n1
+    )
     image_url=${image_url/thumb-/wallpaper-}
     image_url=${image_url/thumbs/wallpapers}
     if [[ ! -z $image_url ]]; then
@@ -74,7 +101,11 @@ http.get.url.nrlmry.nexsat(){
     local path='SouthAmerica/Overview' # NW_Atlantic/Caribbean
     local BASE_URL='http://www.nrlmry.navy.mil/nexsat-bin/nexsat.cgi?BASIN=CONUS&SUB_BASIN=focus_regions&AGE=Archive&REGION='"${region}"'&SECTOR=Overview&PRODUCT=vis_ir_background&SUB_PRODUCT=goes&PAGETYPE=static&DISPLAY=single&SIZE=Thumb&PATH='"${path}"'/vis_ir_background/goes&&buttonPressed=Archive'
     local IMAGE_BASE_URL='http://www.nrlmry.navy.mil/htdocs_dyn_apache/PUBLIC/nexsat/thumbs/full_size/'"${path}"'/vis_ir_background/goes/'
-    local image_url=$(wget "${WGET_OPT}" -q -O- "$BASE_URL" | fgrep -m1 option | cut -f2 -d'"')
+    local image_url=$(
+        wget "${WGET_OPT}" -q -O- "$BASE_URL" | 
+        fgrep -m1 option | 
+        cut -f2 -d'"'
+    )
     if [[ ! -z $image_url ]]; then
         echo "${IMAGE_BASE_URL}"/"${image_url}"
     fi
@@ -82,79 +113,103 @@ http.get.url.nrlmry.nexsat(){
 
 # reference: https://gist.github.com/alexander-yakushev/5546599
 http.get.url.4walled(){
-	# board=
-	#	1 -- /w/   -- Anime/Wallpapers
-	#	2 -- /wg/  -- Wallpapers/General
-	#	3 -- 7chan -- 7chan
-	#	4 -- /hr/  -- NSFW/High Resolution
-	#         --       -- ALL
-	local board=
-
-	# sfw=
-	#	-1 -- unrated
-	#	 0 -- Safe for work
-	#        1 -- Borderline
-	#        2 -- NSFW
-	local sfw=0
-
-	local URL='http://4walled.cc/search.php?tags=&board'${board}'=&width_aspect=1024x133&searchstyle=larger&sfw='"${sfw}"'&search=random'
-	local BASE_URL=$(wget "${WGET_OPT}" -q -O- "${URL}" | fgrep -m1 '<li class' | cut -f4 -d"'")
-	local image_url=$(wget "${WGET_OPT}" -O- -q "${BASE_URL}" | fgrep -m1 'href="http' | cut -f2 -d'"')
-	if [[ ! -z $image_url ]]; then
-		echo "${image_url}"
-	fi
+    # board=
+    #	1 -- /w/   -- Anime/Wallpapers
+    #	2 -- /wg/  -- Wallpapers/General
+    #	3 -- 7chan -- 7chan
+    #	4 -- /hr/  -- NSFW/High Resolution
+    #         --       -- ALL
+    local board=
+    
+    # sfw=
+    #	-1 -- unrated
+    #	 0 -- Safe for work
+    #        1 -- Borderline
+    #        2 -- NSFW
+    local sfw=0
+    
+    local URL='http://4walled.cc/search.php?tags=&board'${board}'=&width_aspect=1024x133&searchstyle=larger&sfw='"${sfw}"'&search=random'
+    local BASE_URL=$(
+        wget "${WGET_OPT}" -q -O- "${URL}" | 
+        fgrep -m1 '<li class' | 
+        cut -f4 -d"'"
+    )
+    local image_url=$(
+        wget "${WGET_OPT}" -O- -q "${BASE_URL}" | 
+	fgrep -m1 'href="http' | 
+	cut -f2 -d'"'
+    )
+    if [[ ! -z $image_url ]]; then
+        echo "${image_url}"
+    fi
 }
 
 # Reference: https://github.com/dmacpherson/py-interfacelift-downloader
 http.get.url.interfacelift(){
-	local BASE_URL='http://interfacelift.com/wallpaper/downloads/random/fullscreen/1600x1200/'
-	local IMAGE_BASE_URL='http://interfacelift.com'
-	local image_url=$(wget "${WGET_OPT}" --quiet -O - "${BASE_URL}" | egrep -o 'a href="/wallpaper/[[:alnum:]]+/[[:alnum:]_]+.jpg' | cut -f2 -d'"' | shuf -n1)
-	if [[ ! -z $image_url ]]; then
-		echo "${IMAGE_BASE_URL}""${image_url}"
-	fi
+    local BASE_URL='http://interfacelift.com/wallpaper/downloads/random/fullscreen/1600x1200/'
+    local IMAGE_BASE_URL='http://interfacelift.com'
+    local image_url=$(
+        wget "${WGET_OPT}" --quiet -O - "${BASE_URL}" | 
+        egrep -o 'a href="/wallpaper/[[:alnum:]]+/[[:alnum:]_]+.jpg' | 
+        cut -f2 -d'"' | 
+        shuf -n1
+    )
+    if [[ ! -z $image_url ]]; then
+        echo "${IMAGE_BASE_URL}""${image_url}"
+    fi
 }
 
 # https://gist.github.com/JoshSchreuder/882666
 http.get.url.nasa.apod(){
-        local BASE_URL='http://apod.nasa.gov/apod/'
-	local IMAGE_BASE_URL=$BASE_URL
-	local image_url=$(wget "${WGET_OPT}" --quiet -O - "${BASE_URL}" | fgrep -m1 jpg | cut -f2 -d'"')
-	if [[ ! -z $image_url ]]; then
-		echo "${IMAGE_BASE_URL}""${image_url}"
-	fi
+    local BASE_URL='http://apod.nasa.gov/apod/'
+    local IMAGE_BASE_URL=$BASE_URL
+    local image_url=$(
+        wget "${WGET_OPT}" --quiet -O - "${BASE_URL}" | 
+        fgrep -m1 jpg | 
+        cut -f2 -d'"'
+    )
+    if [[ ! -z $image_url ]]; then
+        echo "${IMAGE_BASE_URL}""${image_url}"
+    fi
 }
 
 # https://gist.github.com/JoshSchreuder/882668
-http.get.url.netgeo(){
-	local BASE_URL='http://photography.nationalgeographic.com/photography/photo-of-the-day/'
-	local IMAGE_BASE_URL='images.nationalgeographic.com'
-	local image_url=$(wget "${WGET_OPT}" --quiet -O - "${BASE_URL}" | egrep -o -m1 "${IMAGE_BASE_URL}"'/.*[0-9]*x[0-9]*.jpg')
-	if [[ ! -z $image_url ]]; then
-		echo 'http://'"${image_url}"
-	fi
+http.get.url.natgeo(){
+    local BASE_URL='http://photography.nationalgeographic.com/photography/photo-of-the-day/'
+    local IMAGE_BASE_URL='images.nationalgeographic.com'
+    local image_url=$(
+        wget "${WGET_OPT}" --quiet -O - "${BASE_URL}" |  
+        egrep -o -m1 "${IMAGE_BASE_URL}"'/.*[0-9]*x[0-9]*.jpg' 
+    )
+    if [[ ! -z $image_url ]]; then
+        echo 'http://'"${image_url}"
+    fi
 }
 
 http.get.url.nasa.goes(){
-	local BASE_URL='http://goes.gsfc.nasa.gov/goescolor/goeseast/overview2/color_lrg'
-	local image_url="${BASE_URL}"/latestfull.jpg
-	echo "${image_url}"
+    local BASE_URL='http://goes.gsfc.nasa.gov/goescolor/goeseast/overview2/color_lrg'
+    local image_url="${BASE_URL}"/latestfull.jpg
+    echo "${image_url}"
 }
 
 http.get.url.fvalk(){
-	local BASE_URL='http://www.fvalk.com/images/Day_image/?M=D'
-	local IMAGE_BASE_URL='http://www.fvalk.com/images/Day_image/'
-	local dust='GOES-12'
-	local image_url=$(wget "${WGET_OPT}" --quiet -O - "${BASE_URL}" | grep -m1 "${dust}" | cut -f6 -d'"')
-	if [[ ! -z $image_url ]]; then
-		echo "${IMAGE_BASE_URL}""${image_url}"
-	fi
+    local BASE_URL='http://www.fvalk.com/images/Day_image/?M=D'
+    local IMAGE_BASE_URL='http://www.fvalk.com/images/Day_image/'
+    local dust='GOES-12'
+    local image_url=$(
+        wget "${WGET_OPT}" --quiet -O - "${BASE_URL}" | 
+	grep -m1 "${dust}" | 
+	cut -f6 -d'"'
+    )
+    if [[ ! -z $image_url ]]; then
+        echo "${IMAGE_BASE_URL}""${image_url}"
+    fi
 }
 
 http.get.url.smn.satopes(){
-	local BASE_URL='http://www.smn.gov.ar/pronos/imagenes'
-	local image_url="${BASE_URL}"/satopes.jpg
-	echo "${image_url}"
+    local BASE_URL='http://www.smn.gov.ar/pronos/imagenes'
+    local image_url="${BASE_URL}"/satopes.jpg
+    echo "${image_url}"
 }
 
 # Reference: http://awesome.naquadah.org/wiki/NASA_IOTD_Wallpaper
@@ -162,41 +217,41 @@ http.get.url.smn.satopes(){
 # That's why I am using some logic to detect that and "base64" to store the gzip file in a variable.
 http.get.url.nasa.iotd(){
     check_in_path 'base64'
-	local BASE_URL='http://www.nasa.gov/rss/dyn/lg_image_of_the_day.rss'
-	local html_url=$(base64 <(wget "${WGET_OPT}" --quiet -O - --header='Accept-Encoding: gzip' "${BASE_URL}"))
-	local ftype=$(echo "$html_url" | base64 --decode | file -)
-	
-	if [[ $ftype == *gzip* ]]; then
-		html_url=$(echo "$html_url" | base64 --decode | gunzip -)
-	else
-		html_url=$(echo "$html_url" | base64 --decode )
-	fi
-	
-	image_url=$(echo "$html_url" | fgrep -m1 enclosure | cut -f2 -d'"' )
-
-	if [[ ! -z $image_url ]]; then
-		echo "${image_url}"
-	fi
+    local BASE_URL='http://www.nasa.gov/rss/dyn/lg_image_of_the_day.rss'
+    local html_url=$(base64 <(wget "${WGET_OPT}" --quiet -O - --header='Accept-Encoding: gzip' "${BASE_URL}"))
+    local ftype=$(echo "$html_url" | base64 --decode | file -)
+    
+    if [[ $ftype == *gzip* ]]; then
+        html_url=$(echo "$html_url" | base64 --decode | gunzip -)
+    else
+        html_url=$(echo "$html_url" | base64 --decode )
+    fi
+    	
+    image_url=$(echo "$html_url" | fgrep -m1 enclosure | cut -f2 -d'"' )
+    if [[ ! -z $image_url ]]; then
+        echo "${image_url}"
+    fi
 }
 
 # this will need to be replaced someday with a more custom logic that getopts ~azimut
-while getopts ':hcgiafsmntwbr' opt; do
-	case $opt in
-		g) jpg=$(http.get.url.netgeo) ;;
-		i) jpg=$(http.get.url.nasa.iotd) ;;
-		a) jpg=$(http.get.url.nasa.apod) ;;
-		f) jpg=$(http.get.url.fvalk); FEH_OPT='--bg-max' ;;
-		s) jpg=$(http.get.url.smn.satopes);;
-		c) jpg=$(http.get.url.4walled);;
-		m) jpg=$(http.get.url.nrlmry.nexsat);;
-		n) jpg=$(http.get.url.nasa.goes); FEH_OPT='--bg-max';;
-		t) jpg=$(http.get.url.interfacelift);;
-		w) jpg=$(http.get.url.wallbase);;
-		b) jpg=$(http.get.url.bing);;
-		r) jpg=$(http.get.url.reddit);;
-		h) help.usage;;
-		*) echoerr 'uError: option not supported. '; help.usage; exit 1;;
-	esac
+while getopts ':hcgiafsmntwbrd' opt; do
+    case $opt in
+        g) jpg=$(http.get.url.natgeo) ;;
+        i) jpg=$(http.get.url.nasa.iotd) ;;
+        a) jpg=$(http.get.url.nasa.apod) ;;
+        f) jpg=$(http.get.url.fvalk); FEH_OPT='--bg-max' ;;
+        s) jpg=$(http.get.url.smn.satopes);;
+        c) jpg=$(http.get.url.4walled);;
+        m) jpg=$(http.get.url.nrlmry.nexsat);;
+        n) jpg=$(http.get.url.nasa.goes); FEH_OPT='--bg-max';;
+        t) jpg=$(http.get.url.interfacelift);;
+        w) jpg=$(http.get.url.wallbase);;
+        b) jpg=$(http.get.url.bing);;
+        r) jpg=$(http.get.url.reddit);;
+        d) jpg=$(http.get.url.deviantart);;
+        h) help.usage;;
+        *) echoerr 'uError: option not supported. '; help.usage; exit 1;;
+    esac
 done
 
 mkdir -p pics # portability
@@ -204,11 +259,11 @@ cd pics
 
 # referece: http://blog.yjl.im/2012/03/downloading-only-when-modified-using_23.html
 if [[ ! -z $jpg ]]; then
-	pic_name=${jpg##*/}
-	filename="${PWD}/${pic_name}"
-	wget "${WGET_OPT}" "${jpg}" --server-response --timestamping --no-verbose --ignore-length
-	DISPLAY=:0.0 feh "${FEH_OPT}" "${filename}"
-	echo
-	echo 'URL:  '"${jpg}"
-	echo 'FILE: '"${filename}"
+    pic_name=${jpg##*/}
+    filename="${PWD}/${pic_name}"
+    wget "${WGET_OPT}" "${jpg}" --server-response --timestamping --no-verbose --ignore-length
+    DISPLAY=:0.0 feh "${FEH_OPT}" "${filename}"
+    echo
+    echo 'URL:  '"${jpg}"
+    echo 'FILE: '"${filename}"
 fi
